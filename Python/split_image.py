11d5bd3cb2ec5472ac8ae50a447bb6d77ae34bcb
AidenJeong <muindo@clus.org>
2016-12-06 10:01:05 +0900

    이미지 자르기 스크립트 버그 수정

--- Python/split_image.py
+++ Python/split_image.py
@@ -1,3 +1,4 @@
+﻿#-*- coding: utf-8 -*-
 import zipfile
 import os
 import sys
@@ -62,31 +63,43 @@ def img_process(zipfile, imgpath, destdir, isForward = True) :
     if not os.path.isdir(destdir) :
         os.mkdir(destdir)
         
+    #print(zipfile)   
     cropdir = './temp/crop'
     if not os.path.isdir(cropdir) :
         os.mkdir(cropdir)
+    zipbasefile = os.path.basename(zipfile)
+    cropdir = cropdir + '/' + zipbasefile
+    #print(cropdir)
+    if not os.path.isdir(cropdir) :
+        os.mkdir(cropdir)
+    
+    imgpath = imgpath + zipbasefile
     
     for (path, dir, files) in os.walk(imgpath) :
         if path != imgpath :
             break
         for file in files :
             fullpath = os.path.join(path, file)
-            filename = os.path.basename(file)
-            ext = os.path.splitext(file)[1]
+            filename = os.path.splitext(file)[0]
+            ext = os.path.splitext(file)[1].lower()
             if ext not in ('.png', '.jpg', '.jpeg', '.gif') :
-                print("%s file is not image." % file)
+                #file = file.encode('cp949', 'ignore')
+                #print(u"%s file is not image." % file)
                 continue
-            
-            width, height = get_image_size(fullpath)
+            #print(fullpath)
+            #width, height = get_image_size(fullpath)
+            img = Image.open(fullpath)
+            width = img.size[0]
+            height = img.size[1]
             #print(width)
             if width > height :
-                print('split image : %s' % file)
+                #print(u'split image : %s' % file)
                 lhs = '_01'
                 rhs = '_02'
                 if isForward == False :
                     lhs = '_02'
                     rhs = '_01'
-                img = Image.open(fullpath)
+                #img = Image.open(fullpath)
                 halfwidth = width / 2
                 left_img = img.crop((0, 0, halfwidth, height))
                 right_img = img.crop((halfwidth, 0, width, height))
@@ -95,17 +108,17 @@ def img_process(zipfile, imgpath, destdir, isForward = True) :
                 left_img.save(os.path.join(cropdir, left_filename))
                 right_img.save(os.path.join(cropdir, right_filename))
             else :
-                print('copy file : %s' % file)
+                #print(u'copy file : %s' % file)
                 shutil.copy(fullpath, cropdir)
                 
     # zip
-    outfile = os.path.basename(zipfile)
+    outfile = os.path.basename(zipfile) + '.zip'
     zip(cropdir, os.path.join(destdir, outfile))
     
 def isZipFile(filename) :
     ext = os.path.splitext(filename)[1]
     if ext != '.zip' :
-        print('%s file is not zip file.' % filename)
+        print(u'%s file is not zip file.' % filename)
         return False
     return True
 
@@ -113,37 +126,43 @@ def main(src, dest, isForward = True) :
     isfile = os.path.isfile(src)
     if isfile == False :
         if os.path.exists(src) == False :
-            print('wrong parameter. (%s)' % src)
+            print(u'wrong parameter. (%s)' % src)
             return
             
     tempdir = './temp'
+    unzipdir = tempdir + '/unzip/'
     
     if isfile :
         if isZipFile(src) :
-            unzip(src, tempdir)
-            img_process(src, tempdir, dest, isForward)
-            remove_all(tempdir)
+            unzip(src, unzipdir)
+            img_process(src, unzipdir, dest, isForward)
     else :
         for (path, dir, files) in os.walk(src) :
             for file in files :
                 if isZipFile(file) == False :
                     continue
+                print('Unzip file... %s' % file)
                 fullpath = os.path.join(path, file)
-                unzip(fullpath, tempdir)
-                img_process(fullpath, tempdir, dest, isForward)
-                remove_all(tempdir)
+                unzip(fullpath, unzipdir + os.path.splitext(file)[0])
+                #img_process(fullpath, tempdir, dest, isForward)
+        for dir in os.listdir(unzipdir) :
+            fulldir = os.path.join(unzipdir, dir)
+            print('Processing file... %s' % dir)
+            img_process(fulldir, unzipdir, dest, isForward)
+                
+    remove_all(tempdir)
                 
 def useage() :
-    print("python split_image.py [origin] [dest] [direction]\n")
-    print("\t[origin]\tzipfile or directory.")
-    print("\t[dest]\t\toutput directory.")
-    print("\t[direction]\tRead direction.")
-    print("\t\t\tLeft to right is 'R'")
-    print("\t\t\tRight to left is 'L'")
+    print(u"python split_image.py [origin] [dest] [direction]\n")
+    print(u"\t[origin]\tzipfile or directory.")
+    print(u"\t[dest]\t\toutput directory.")
+    print(u"\t[direction]\tRead direction.")
+    print(u"\t\t\tLeft to right is 'R'")
+    print(u"\t\t\tRight to left is 'L'")
 
 if __name__ == "__main__" :
     if len(sys.argv) < 4 :
-        print("Wrong useage.\n")
+        print(u"Wrong useage.\n")
         useage()
     else :
         origin = sys.argv[1]
@@ -156,7 +175,7 @@ if __name__ == "__main__" :
             isForward = True
             main(origin, dest, isForward)
         else :
-            print("Wrong useage.\n")
+            print(u"Wrong useage.\n")
             useage()
             
        
